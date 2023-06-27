e807260cbd99363a1ef4fda4746c4c49d01d73dc
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-06-27 15:56:23 +0530

    Relationships-1-Update-1

--- relationship/src/main/java/lk/ijse/dep10/relationship/BiDirectionalGotchas3.java
+++ relationship/src/main/java/lk/ijse/dep10/relationship/BiDirectionalGotchas3.java
@@ -1,20 +1,19 @@
 package lk.ijse.dep10.relationship;
 
 import lk.ijse.dep10.relationship.entity.Customer;
-import lk.ijse.dep10.relationship.entity.Order;
 import lk.ijse.dep10.relationship.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
-public class BiDirectionalGotchas2 {
+public class BiDirectionalGotchas3 {
     public static void main(String[] args) {
         try(SessionFactory sf = HibernateUtil.getSessionFactory();
             Session session = sf.openSession()){
 
             session.beginTransaction();
 
-            Customer kasun = session.get(Customer.class, 1);
-            kasun.getOrderList().remove(1);
+            Customer ruwan = session.get(Customer.class, 3);
+            session.remove(ruwan);
 
 
 
--- relationship/src/main/java/lk/ijse/dep10/relationship/BiDirectionalGotchas4.java
+++ relationship/src/main/java/lk/ijse/dep10/relationship/BiDirectionalGotchas4.java
@@ -1,9 +1,14 @@
 package lk.ijse.dep10.relationship;
 
 import lk.ijse.dep10.relationship.entity.Customer;
+import lk.ijse.dep10.relationship.entity.Order;
 import lk.ijse.dep10.relationship.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.List;
 
 public class BiDirectionalGotchas4 {
     public static void main(String[] args) {
@@ -12,7 +17,23 @@ public class BiDirectionalGotchas4 {
 
             session.beginTransaction();
 
-            Customer kasun = session.get(Customer.class, 1);
+            Customer ruwan = new Customer(3, "Ruwan", "089-1234567");
+            Customer supun = new Customer(4, "Supun", "084-1234567");
+
+            List<Order> ruwanOrderList = List.of(
+                    new Order(4, Date.valueOf(LocalDate.now()), ruwan),
+                    new Order(5, Date.valueOf(LocalDate.now()), ruwan));
+            ruwan.setOrderList(ruwanOrderList);
+
+            List<Order> supunOrderList = List.of(
+                    new Order(6, Date.valueOf(LocalDate.now()), ruwan),
+                    new Order(7, Date.valueOf(LocalDate.now()), supun));
+            supun.setOrderList(supunOrderList);
+
+            session.persist(ruwan);
+            session.persist(supun);
+
+            System.out.println(supunOrderList.get(0));    // Ruwan -> Supun
 
 
 
