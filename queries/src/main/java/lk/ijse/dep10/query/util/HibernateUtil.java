14419b5e00ad1f7e0978b75a7a3e3d2fc9ce9601
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-07-04 16:26:22 +0530

    Project setup for query

---
+++ queries/src/main/java/lk/ijse/dep10/query/util/HibernateUtil.java
@@ -0,0 +1,38 @@
+package lk.ijse.dep10.query.util;
+
+import lk.jise.dep10.rel.entity.*;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.model.naming.ImplicitNamingStrategyJpaCompliantImpl;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory(){
+        StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+                .loadProperties("application.properties")
+                .build();
+
+        Metadata metadata = new MetadataSources(standardRegistry)
+                .addAnnotatedClasses(Actor.class, ActorMovie.class, Movie.class)
+                .addAnnotatedClasses(Student.class, Course.class, StudentCourse.class)
+                .addAnnotatedClasses(Customer.class, Order.class, CustomerOrder.class)
+                .getMetadataBuilder()
+                .applyImplicitNamingStrategy(ImplicitNamingStrategyJpaCompliantImpl.INSTANCE)
+                .build();
+
+        return metadata.getSessionFactoryBuilder()
+                .build();
+    }
+
+    public static SessionFactory getSessionFactory(){
+        return sessionFactory;
+    }
+
+
+
+
+}
